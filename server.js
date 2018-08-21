49fd5ef973b030f831cf4ea43e8ab6a40b6ab6fb
Extrasy <luisalexpm@gmail.com>
2018-08-20 17:29:58 -0700

    file updates

---
+++ server.js
@@ -0,0 +1,39 @@
+
+// DEPENDENCIES
+var express = require('express');
+
+var bodyParser = require('body-parser');
+
+var path = require('path');
+
+
+// EXPRESS CONFIGURATION
+var app = express(); // Tells node that we are creating an "express" server
+
+var PORT = process.env.PORT || 3000; // Sets an initial port.
+
+// BodyParser makes it easy for our server to interpret data sent to it.
+
+// The code below is pretty standard.
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.text());
+
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+// ================================================================================
+
+// ROUTER
+require('./app/routing/api-routes.js')(app); 
+
+require('./app/routing/html-routes.js')(app);
+
+// LISTENER
+// start server
+app.listen(PORT, function() {
+
+  console.log("App listening on PORT: " + PORT);
+
+});
